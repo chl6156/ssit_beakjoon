e1a423e005358dec3d991eeb7bc945616cd6ca32
chl6156 <chl6155@naver.com>
2024-11-27 13:08:37 +0900

    [Gold I] Title: 팰린드롬 진법, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/6757. 팰린드롬 진법/팰린드롬 진법.py
@@ -0,0 +1,29 @@
+# 필랜드롬 진법
+
+X = int(input())
+p = [0] * 40  
+
+# 첫 번째 루프: X를 b진법으로 변환하여 팰린드롬인지 검사
+for i in range(2, int(X**0.5) + 1):
+    l = 0
+    k = X
+    while k > 0:
+        p[l] = k % i
+        k //= i
+        l += 1
+
+    j, k = 0, l - 1
+    while j < k:
+        if p[j] != p[k]:
+            break
+        j += 1
+        k -= 1
+
+    if j >= k and l > 1:
+        print(i)
+
+# 두 번째 루프: temp * b + b = X 조건 확인
+for i in range(int(X**0.5), 0, -1):
+    if (X - i) % i == 0 and (X - i) // i > i:
+        print((X - i) // i)
+
